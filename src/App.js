a5ceebaf5dfcd0f2e5ccc4bd178d2042c1734ef3
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-01-24 00:06:58 +0100

    convert

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import About from "./About";
+import About from "./components/About";
 import "./App.css";
-import Footer from "./Footer";
-import Hero from "./Hero";
-import NavBar from "./NavBar";
-import Services from "./Services";
-import Tours from "./Tours";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
 
 function App() {
   return (
