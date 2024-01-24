9b55d1f6c28a1fd2280f2b6659d7fd7ec96a981d
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-01-24 15:33:05 +0100

    completed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className='hero' id='home'>
       <div className='hero-banner'>
-        <h1>LET'S GO TO BARRACUDA</h1>
+        <h1>Capitano, LET'S GO TO BARRACUDA</h1>
         <p>
           Let us go and enjoy at Barracuda beach where we will hang out and do
           alot of stuffs
